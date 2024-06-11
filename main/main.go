3d1234b2239c78ff26bf52f67a0b7cf093d5d502
gkopoin <gab270b@gmail.com>
2024-06-11 14:00:05 +0200

    done

--- main/main.go
+++ main/main.go
@@ -12,15 +12,15 @@ func main() {
 	data := skills.Read_File(os.Args[1])
 
 	average := skills.Average(data)
-	fmt.Println("Average : ", math.Round(average))
+	fmt.Println("Average:", math.Round(average))
 
 	median := skills.Median(data)
-	fmt.Println("Median : ", math.Round(median))
+	fmt.Println("Median:", math.Round(median))
 
 	variance := skills.Variance(data)
 	conv := strconv.FormatInt(int64(math.Round(variance)), 10)
-	fmt.Println("Variance : ", conv)
+	fmt.Println("Variance:", conv)
 
-	standart_variation := skills.Standart(data)
-	fmt.Println("Standart Variation : ", math.Round(standart_variation))
+	standard_variation := skills.Standard(data)
+	fmt.Println("Standard Variation:", math.Round(standard_variation))
 }
